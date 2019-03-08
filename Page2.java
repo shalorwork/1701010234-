31df5cf3b389859eb4d98925cb8e9e070487e0d0
shalorwork <shelley_xxr@163.com>
2019-03-08 09:48:50 +0800

    测试

---
+++ Page2.java
@@ -0,0 +1,27 @@
+
+public class Page2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		int k=0;
+		System.out.println("hello");
+		while(sum<50000){
+			k++;
+			for(int i=0;i<100;i++){
+				sum=sum+1;
+				System.out.println("sum="+sum);
+				System.out.println("i="+i);
+			
+			}
+		System.out.println("k="+k);
+		System.out.println("sum="+sum);
+		
+		}
+		
+	}
+
+}
